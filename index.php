9c4f5e19e3db3b093c7c0e7000318203e9ba725d
Jérôme Brachet <brachet.jerome@gmail.com>
2017-12-01 13:03:50 +0100

    Projet AnnuEure

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
-<?php $title ='home' ?>
-<!-- ==========================================================
-    // PAGE INDEX
-    ========================================================== -->
+<?php
+/*=========================================================
+Require autoload.php => ajout des librairies php avec Composer
+Utilisation de la librairie PHP Paginator
+==========================================================*/
+require('vendor/autoload.php');
+use JasonGrimes\Paginator;
+/*=========================================================
+Inclusion de la Classe PDO => pdo.php
+Inclusion de mes fonctions => fonctions.php
+==========================================================*/
+include('inc/pdo.php');
+include('inc/fonctions.php');
+?>
+
+<?php $title ='Accueil' ?>
+
 <!-- Include Entête -->
 <?php include('inc/header.php'); ?>
+
 <!-- Début index -->
 <h1>Page Accueil</h1>
 
